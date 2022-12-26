2fc5226b2eaf7172a8864f2883f38b32ba99d5bd
jamey <101050942+catturtle123@users.noreply.github.com>
2022-12-26 22:06:08 +0900

    Create mysolution.py

---
+++ 그리디/mysolution.py
@@ -0,0 +1,21 @@
+n,m,k = map(int,input().split()) # 나누어서 입력 받아서 변수로 저장한다.
+u = 0
+
+numList = list(map(int,input().split())) # 나누어서 입력 받아서 리스트로 저장한다.
+
+for i in range(len(numList)-1,0,-1): # bubbleSort로 정렬한다.
+    for j in range(i):
+        if numList[j] > numList[j+1]:
+            numList[j], numList[j+1] = numList[j+1],numList[j]
+
+sum = 0 # 합계를 설정한다.
+
+while u < m:
+    for i in range(k): # 가장 큰 숫자를 횟수만큼 더한다.
+        sum += numList[len(numList)-1]
+        u += 1
+    
+    sum += numList[len(numList)-2] # 반복 제한이 있기에 두번째로 큰 수를 더해준다.
+    u += 1
+    
+print(sum) # 합계를 출력한다.
