f38b953c49d0dfaaea984d6fa53805e5ed25c084
jamey <101050942+catturtle123@users.noreply.github.com>
2023-10-31 13:46:10 +0900

    [Silver III] Title: 이전 순열, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for(auto i = a; i < b; ++i)
+#define rrep(i, a, b) for(auto i = a; i > b; --i)
+#define REP(i, a, b) for(auto i = a; i <= b; ++i)
+#define RREP(i, a, b) for(auto i = a; i >= b; --i)
+#define pii pair<int, int>
+#define tii tuple<int, int, int>
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define INF numeric_limits<int>::max()
+#define PIV (1 << 20)
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+#define FAST_IO \
+    if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
+#define debug(x) \
+    if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugf(...) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "; printf(__VA_ARGS__); }
+#define debugc(c) \
+    if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+vector<int> result;
+vector<int> start;
+
+int main(void) {
+    FAST_IO;   
+    int n;
+    cin >> n;
+    result.resize(n);
+    start.resize(n);
+    rep(i, 0, n) cin >> result[i];
+    rep(i, 0, n) start[i] = i + 1;
+
+    if (result == start) {
+        cout << -1 << endl;
+    } else {
+        prev_permutation(result.begin(),result.end());
+        rep(i, 0, n) cout << result[i] << ' ';
+         cout << endl;
+    }
+   
+
+    return 0;
+}
