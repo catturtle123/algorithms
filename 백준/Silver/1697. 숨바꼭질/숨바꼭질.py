6ea1047fb72b6dda1e79c66410e51690d825a6d4
jamey <101050942+catturtle123@users.noreply.github.com>
2025-10-27 12:25:04 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 37612 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+INF = sys.maxsize
+
+def bfs(start):
+    queue = deque([start])
+
+    while queue:
+        pos, length = queue.popleft()
+
+        if 0 <= pos + 1 <= 100_000:
+            if dp[pos + 1] == INF:
+                dp[pos + 1] = min(dp[pos + 1], length + 1)
+                queue.append((pos + 1, dp[pos + 1]))
+        
+        if 0 <= pos - 1 <= 100_000:
+            if dp[pos - 1] == INF:
+                dp[pos - 1] = min(dp[pos - 1], length + 1)
+                queue.append((pos - 1, dp[pos - 1]))
+        
+        if 0 <= 2 * pos <= 100_000:
+            if dp[2 * pos] == INF:
+                dp[2 * pos] = min(dp[pos * 2], length + 1)
+                queue.append((2 * pos, dp[2 * pos]))
+
+n, k = map(int, input().split())
+
+dp = [INF] * (100_001)
+dp[n] = 0
+
+bfs((n, 0))
+
+print(dp[k])
