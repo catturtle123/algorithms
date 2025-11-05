c9a65a138d256d42f885b8b2ddb9c0411bc787a4
jamey <101050942+catturtle123@users.noreply.github.com>
2025-11-05 11:32:02 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 76 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,51 +1,51 @@
-import sys, copy
+import sys
 input = sys.stdin.readline
 
+
+def board_search(start):
+    y = start[0]
+    x = start[1]
+    start_str = start[2]
+
+    result = 0
+    # 시작 부터 8칸 탐색
+    for i in range(y, y + 8):
+        for j in range(x, x + 8):
+            current = board[i][j]
+
+            # 짝수일 때 다르면 +1
+            if (i + j - y - x) % 2 == 0 and start_str != current:
+                result += 1
+            elif (i + j - y - x) % 2 != 0 and start_str == current:
+                result += 1
+
+              
+    return result
+
 n, m = map(int, input().split())
-
-def count_board(y, x):
-    mins = sys.maxsize
-    pattern = ["W", "B"]
-
-    for start_color in pattern:
-        fail = 0
-        if start_color == "W":
-            for i in range(8):
-                for j in range(8):
-                    if (i + j) % 2 == 0:
-                        if board[y + i][x + j] != "W":
-                            fail += 1
-                    if (i + j) % 2 == 1:
-                        if board[y + i][x + j] != "B":
-                            fail += 1
-        else:
-            for i in range(8):
-                for j in range(8):
-                    if (i + j) % 2 == 0:
-                        if board[y + i][x + j] != "B":
-                            fail += 1
-                    if (i + j) % 2 == 1:
-                        if board[y + i][x + j] != "W":
-                            fail += 1
-
-        mins = min(mins, fail)
-
-    return mins
-
 board = []
+
 for i in range(n):
     s = input().rstrip()
     temp = []
     for j in s:
         temp.append(j)
+
     board.append(temp)
 
-mins = sys.maxsize
+min_result = sys.maxsize
 for i in range(n):
     for j in range(m):
-        if 0 <= i <= n and 0 <= j <= m and 0 <= i + 8 <= n and 0 <= j + 8 <= m:
-            fail = count_board(i, j)
-            if mins > fail:
-                mins = fail
+        if i + 8 > n or j + 8 > m:
+            continue
 
-print(mins)
+        result = board_search((i, j, "W"))
+        if min_result >  result:
+            min_result = result
+
+        result = board_search((i, j, "B"))
+        if min_result >  result:
+            min_result = result
+
+print(min_result)
+           
