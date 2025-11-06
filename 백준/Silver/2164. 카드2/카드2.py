01872dac3054b7e06422e9f9b66a7bf95691c0e4
jamey <101050942+catturtle123@users.noreply.github.com>
2025-11-06 11:24:55 +0900

    [Silver IV] Title: 카드2, Time: 204 ms, Memory: 55960 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -2,13 +2,12 @@ from collections import deque
 
 n = int(input())
 
-nlist = list(range(1,n+1))
-dequeData = deque(nlist)
+nlist = [i for i in range(1, n + 1)]
+nqueue = deque(nlist)
 
-while len(dequeData) != 1:
-    dequeData.popleft()
-    dequeData.insert(len(dequeData),dequeData[0])
-    dequeData.popleft()
+while len(nqueue) > 1:
+    nqueue.popleft()
+    q = nqueue.popleft()
+    nqueue.append(q)
 
-
-print(dequeData[0])
+print(nqueue[0])
