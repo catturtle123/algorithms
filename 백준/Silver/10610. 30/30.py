1686cca1c22a81a37356d9139e931904d17bd152
jamey <101050942+catturtle123@users.noreply.github.com>
2025-11-08 09:33:21 +0900

    [Silver IV] Title: 30, Time: 92 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.py
+++ 백준/Silver/10610. 30/30.py
@@ -1,9 +1,24 @@
-s_num = list(map(int, input()))
+n = list(input())
 
-s_num.sort(reverse=True)
+def is_thrity():
+    is_zero = False
+    sums = 0
+    for i in n:
+        if i == '0':
+            is_zero = True
+        sums += int(i)
 
-if sum(s_num) % 3 != 0 or s_num[-1] != 0:
-    print(-1)
-else:
-    for i in s_num:
-        print(i,end="")
+    if not is_zero:
+        print(-1)
+        return False
+
+    if sums % 3 != 0:
+        print(-1)
+        return False
+
+    return True
+
+if is_thrity():
+    n.sort(reverse=True)
+    for i in n:
+        print(i, end="")
