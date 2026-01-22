6c73873cd4ef4bd2c9fa536b4ac9c695f8954ea0
jamey <101050942+catturtle123@users.noreply.github.com>
2026-01-22 23:41:33 +0900

    [Bronze V] Title: 세제곱의 합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+result = 0
+for i in range(1, n + 1):
+       result += i
+
+print(result)
+
+result = 0
+for i in range(1, n + 1):
+       result += i
+
+print(pow(result,2))
+
+result = 0
+for i in range(1, n + 1):
+       result += pow(i, 3)
+
+print(result)
