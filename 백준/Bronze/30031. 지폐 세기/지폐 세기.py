12cfea6020dce0438e3192cb63d7ed88d8ccc9a5
jamey <101050942+catturtle123@users.noreply.github.com>
2026-03-27 12:55:10 +0900

    [Bronze IV] Title: 지폐 세기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30031. 지폐 세기/지폐 세기.py
@@ -0,0 +1,17 @@
+# 136, 142, 148, 154
+n = int(input())
+
+result = 0
+for _ in range(n):
+    a, b = map(int, input().split())
+    
+    if a == 136:
+        result += 1000
+    elif a == 142:
+        result += 5000
+    elif a == 148:
+        result += 10000
+    elif a == 154:
+        result += 50000
+
+print(result)
