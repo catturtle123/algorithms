565c7159248e2fb1b089c1afdb299cda6068a0bf
jamey <101050942+catturtle123@users.noreply.github.com>
2026-04-09 16:58:46 +0900

    [Silver IV] Title: 수 찾기, Time: 440 ms, Memory: 49152 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,33 +1,31 @@
 import sys
+input = sys.stdin.readline
 
-def binary_search(li, x):
+def binary_search(nlist, x):
     start = 0
-    end = len(li) - 1
+    end = len(nlist) - 1
 
     while start <= end:
         mid = (start + end) // 2
 
-        if li[mid] == x:
-            return mid
-        elif li[mid] > x:
-            end = mid - 1
+        if nlist[mid] == x:
+            return True
+        elif nlist[mid] < x:
+            start = mid + 1
         else:
-           start = mid + 1
+            end = mid - 1
     
-    return None
-
-
-input = sys.stdin.readline
+    return False
 
 n = int(input())
 nlist = list(map(int, input().split()))
+
 m = int(input())
-mlist = list(map(int, input().split()))
+mlist = list(map(int, input().split())) # 여기 숫자 중 nlist에 존재하면 1 아니면 0
 
 nlist.sort()
-
 for i in mlist:
-    if binary_search(nlist, i) == None:
-      print(0)
+    if binary_search(nlist, i):
+        print(1)
     else:
-      print(1)
+        print(0)
