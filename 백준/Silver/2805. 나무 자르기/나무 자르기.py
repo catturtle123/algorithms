f459ce8ddb1714ee02cce5d8c9489ddcfe7ce780
jamey <101050942+catturtle123@users.noreply.github.com>
2026-04-11 13:56:59 +0900

    [Silver II] Title: 나무 자르기, Time: 2204 ms, Memory: 148224 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,28 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-def binary_search(start, end):
-    result = 0
-    while start <= end: 
+def binary_search(nlist, x):
+    start = 0
+    end = nlist[-1]
+
+    while start <= end:
         mid = (start + end) // 2
 
-        sums = 0
+        temp = 0
         for i in nlist:
-            temp = i - mid
-            if temp > 0:
-                sums += temp
-
-        if sums >= m:
-            result = mid   
+            if i > mid:
+                temp += (i - mid)
+        
+        if temp >= x:
             start = mid + 1
         else:
             end = mid - 1
+    
+    return end
 
-    print(result)  
+
 
 n, m = map(int, input().split())
 nlist = list(map(int, input().split()))
 
-start = 0
-end = max(nlist)
-binary_search(start, end)
+nlist.sort()
+print(binary_search(nlist, m))
