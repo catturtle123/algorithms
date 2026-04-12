89296f3b325b746c1406814a1591fc3a0d3a9425
jamey <101050942+catturtle123@users.noreply.github.com>
2026-04-12 13:49:35 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 228 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -5,10 +5,9 @@ n, m = map(int, input().split())
 nlist = list(map(int, input().split()))
 
 nlist.insert(0, 0)
-for i in range(2, len(nlist)):
-    nlist[i] += nlist[i - 1]
+for i in range(1, len(nlist)):
+    nlist[i] = nlist[i] + nlist[i - 1]
 
-for i in range(m):
-    a, b = map(int, input().split())
-
-    print(nlist[b] - nlist[a - 1])
+for _ in range(m):
+    i, j = map(int, input().split())
+    print(nlist[j] - nlist[i - 1])
