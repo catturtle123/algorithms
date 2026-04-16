794ee1f45679f3100aa5b3eb70cda7a2d4d48aba
jamey <101050942+catturtle123@users.noreply.github.com>
2026-04-16 16:13:31 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 1076 ms, Memory: 160424 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -0,0 +1,29 @@
+import sys, bisect
+input = sys.stdin.readline
+
+n = int(input())
+nlist = list(map(int, input().split()))
+temp_list = []
+temp_list.append(nlist[0])
+pos = [0] * (len(nlist) + 1)
+
+for i in range(1, len(nlist)):
+    if temp_list[-1] < nlist[i]:
+        temp_list.append(nlist[i])
+        pos[i] = len(temp_list) - 1
+    else:
+        temp_right_index = bisect.bisect_left(temp_list, nlist[i])
+        temp_list[temp_right_index] = nlist[i]
+        pos[i] = temp_right_index
+
+target = len(temp_list) - 1
+answer = []
+
+for i in range(n - 1, -1, -1):
+    if pos[i] == target:
+        answer.append(nlist[i])
+        target -= 1
+
+answer.reverse()
+
+print(len(temp_list))  
