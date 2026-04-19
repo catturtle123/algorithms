7881615a46c3201c22695237c3f9b73d0b343f42
jamey <101050942+catturtle123@users.noreply.github.com>
2026-04-19 18:02:21 +0900

    [Bronze IV] Title: Football Team, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5358. Football Team/Football Team.py
@@ -0,0 +1,19 @@
+while True:
+    try:
+        s = input()
+    except EOFError:
+        break
+
+    for i in s:
+        if i == "e":
+            print("i", end="")
+        elif i == "i":
+            print("e", end="")
+        elif i == "E":
+            print("I", end="")
+        elif i == "I":
+            print("E", end="")
+        else:
+            print(i, end="")
+    
+    print()
